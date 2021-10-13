4dcdf32848e04e9f36eeb5690502124b6296c3d2
aleksmaksiuta <aleks.maksiuta@gmail.com>
2021-10-13 10:43:03 +0300

    feat: added modifier for URL;

--- bff-service/app.js
+++ bff-service/app.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(express.json());
 
+const urlModifiers = {
+  cart: (originalUrl, recipientUrl) => {
+    const [,,...rest] = originalUrl.split('/')
+    return `${recipientUrl}/${rest.join('/')}`;
+  }
+}
+
 app.all('/*', (req, res) => {
   const recipient = req.originalUrl.split('/')[1];
 
@@ -15,9 +22,12 @@ app.all('/*', (req, res) => {
   if (!recipientUrl) {
     res.status(502).json({ error: 'Cannot process request' });
   } else {
+    const urlModifier = urlModifiers[recipient];
+    const url = urlModifier ? urlModifier(req.originalUrl, recipientUrl) : `${recipientUrl}${req.originalUrl}`;
+
     const config = {
       method: req.method,
-      url: `${recipientUrl}${req.originalUrl}`,
+      url,
       ...(Object.keys(req.body || {}).length > 0 && { data: req.body }),
     };
 
